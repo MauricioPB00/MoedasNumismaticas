e5a8ccd02d1cb776596cea6819cb3905117db092
MauricioPB00 <mauricioperinbecker@gmail.com>
2025-09-11 19:11:13 -0300

    CREATE: Salvar foto no banco

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -8,6 +8,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SettingsComponent implements OnInit {
   user: any = {};
+  showForm = false;
+  showPhoto = false
+
+  selectedFile: File | null = null;
+previewUrl: string | null = null;
 
   constructor(private http: HttpClient) { }
 
@@ -19,30 +24,69 @@ export class SettingsComponent implements OnInit {
     }
   }
 
-  onSubmit() {
-  const token = localStorage.getItem('jwt'); // pega o JWT
-  console.log(token);
-  if (!token) {
-    alert('Usuário não autenticado');
-    return;
+
+  toggleForm() {
+    this.showForm = !this.showForm;
   }
 
-  const headers = new HttpHeaders({
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json'
-  });
+  togglePhoto() {
+    this.showPhoto = !this.showPhoto;
+  }
 
-  this.http.put<any>('http://localhost:8000/api/update', JSON.stringify(this.user), { headers })
-  .subscribe({
-    next: (res) => {
-      console.log(res);
-      localStorage.setItem('jwt', res.token); // agora o TS aceita
-      alert('Dados atualizados com sucesso!');
-    },
-    error: (err) => {
-      console.error(err);
-      alert('Erro ao atualizar dados');
+  onFileSelected(event: any) {
+  const file: File = event.target.files[0];
+  if (file) {
+    this.selectedFile = file;
+
+    const reader = new FileReader();
+    reader.onload = () => this.previewUrl = reader.result as string;
+    reader.readAsDataURL(file);
+  }
+}
+
+
+  onSubmit() {
+    const token = localStorage.getItem('jwt'); // pega o JWT
+    console.log(token);
+    if (!token) {
+      alert('Usuário não autenticado');
+      return;
     }
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    this.http.put<any>('http://localhost:8000/api/update', JSON.stringify(this.user), { headers })
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          localStorage.setItem('jwt', res.token); // agora o TS aceita
+          alert('Dados atualizados com sucesso!');
+          this.showForm = false;
+        },
+        error: (err) => {
+          console.error(err);
+          alert('Erro ao atualizar dados');
+        }
+      });
+  }
+
+  onSubmitPhoto() {
+  const token = localStorage.getItem('jwt');
+  if (!this.selectedFile) return;
+
+  const formData = new FormData();
+  formData.append('photo', this.selectedFile);
+
+  this.http.post('http://localhost:8000/api/photo', formData, {
+    headers: { Authorization: `Bearer ${token}` }
+  }).subscribe((res: any) => {
+    console.log('Foto salva com sucesso:', res);
+    this.user.photo = res.photo; // atualiza a foto do usuário
+    this.selectedFile = null;
+    this.previewUrl = null;
   });
 }
 
