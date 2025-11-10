905f9f312e48d931486f7b56d4a6667de5cea0fd
MauricioPB00 <mauricioperinbecker@gmail.com>
2025-11-10 09:44:49 -0300

    FIX: Ajuste nos paises

--- src/app/models/countriesCAD.ts
+++ src/app/models/countriesCAD.ts
@@ -6,12 +6,12 @@ export interface CountryCAD {
 }
 
 export const AVAILABLE_COUNTRIES_CAD: CountryCAD[] = [
-  { name: 'Brasil', code: 'br' , coin: 999,banknote:  999},
-  { name: 'Paraguai', code: 'py', coin: 999,banknote:  999 },
-  { name: 'Uruguai', code: 'uy', coin: 999,banknote:  999 },
-  { name: 'Chile', code: 'cl' , coin: 999,banknote:  999},
-  { name: 'Bolivia', code: 'bo' , coin: 999,banknote:  999},
-  { name: 'Peru', code: 'pe', coin: 999,banknote:  999 },
-  { name: 'Argentina', code: 'ar', coin: 999,banknote:  999 },
-  { name: 'Equador', code: 'ec', coin: 999,banknote:  999}
+  { name: 'Brasil', code: 'br' , coin: 655,banknote:  453},
+  { name: 'Paraguai', code: 'py', coin: 325,banknote:  199 },
+  { name: 'Uruguai', code: 'uy', coin: 264,banknote:  155 },
+  { name: 'Chile', code: 'cl' , coin: 318,banknote:  139},
+  { name: 'Bolivia', code: 'bo' , coin: 314,banknote:  126},
+  { name: 'Peru', code: 'pe', coin: 499,banknote:  149 },
+  { name: 'Argentina', code: 'ar', coin: 383,banknote:  306 },
+  { name: 'Equador', code: 'ec', coin: 0,banknote:  0}
 ];
