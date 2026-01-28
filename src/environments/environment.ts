ef8e9233126e9ab519e2c35112851a7a25d719ec
MauricioPB00 <mauricioperinbecker@gmail.com>
2026-01-28 17:37:40 -0300

    FIX: Ajuste para AWS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
 
 
+//LocalHost
+// export const environment = {
+//   API: 'http://127.0.0.1:8000/api',
+//   URL: 'http://localhost:8000/api'
+// };
+
+
+//AWS
 export const environment = {
-  API: 'http://127.0.0.1:8000/api',
-  URL: 'http://localhost:8000/api'
+  API: 'http://172.31.6.216:8000/api',
+  URL: 'http://172.31.6.216:8000/api'
 };
