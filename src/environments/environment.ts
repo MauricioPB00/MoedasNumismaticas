6535eb03885f285d22fd09cf9bbba80694240b4e
MauricioPB00 <mauricioperinbecker@gmail.com>
2026-01-31 20:39:39 -0300

    teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@
 
 //AWS
 export const environment = {
-  API: 'https://albumnumismatico.com.br/api',
-  URL: 'https://albumnumismatico.com.br/api'
+  API: 'https://www.albumnumismatico.com.br/api',
+  URL: 'https://www.albumnumismatico.com.br/api'
 };
