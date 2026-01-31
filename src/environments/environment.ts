d43659f472eb8dcec52f76b6e8db643bceac7abf
MauricioPB00 <mauricioperinbecker@gmail.com>
2026-01-31 16:45:45 -0300

    FIX: Environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+
+//LocalHost
+// export const environment = {
+//   API: 'http://127.0.0.1:8000/api',
+//   URL: 'http://localhost:8000/api'
+// };
+
+
+//AWS
+export const environment = {
+  API: 'https://albumnumismatico.com.br/api',
+  URL: 'https://albumnumismatico.com.br/api'
+};
